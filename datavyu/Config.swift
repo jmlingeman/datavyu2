8b892cff00f5031041465ced0b5e23288a4c322f
Jesse Lingeman <jesse@jesseisageek.com>
2023-06-06 15:28:06 -0400

    add config file and default keybindings

---
+++ datavyu/Config.swift
@@ -0,0 +1,24 @@
+//
+//  Config.swift
+//  datavyu
+//
+//  Created by Jesse Lingeman on 6/6/23.
+//
+
+import Foundation
+
+class Config {
+    let name = "Datavyu"
+    let version = "0.0.1"
+    
+    let minCellHeight = 100
+    let minCellWidth = 100
+    
+    /* Default Keybindings */
+    let playKey = "w"
+    let prevFrameKey = "q"
+    let nextFrameKey = "e"
+    let newCellBlankTimeKey = "a"
+    let newCellAtTimeKey = "s"
+    let pointCellKey = "d"
+}
