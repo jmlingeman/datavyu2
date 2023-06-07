3c2c264816c5884beae6984fb023850b6704e0ea
Jesse Lingeman <jesse@jesseisageek.com>
2023-06-06 22:49:16 -0400

    more script engine stubs

---
+++ datavyu/Scripting/PythonScriptEngine.swift
@@ -0,0 +1,10 @@
+//
+//  PythonScriptEngine.swift
+//  datavyu
+//
+//  Created by Jesse Lingeman on 6/6/23.
+//
+
+import Foundation
+
+// https://github.com/pvieito/PythonKit
---
+++ datavyu/Scripting/RScriptEngine.swift
@@ -0,0 +1,10 @@
+//
+//  RScriptEngine.swift
+//  datavyu
+//
+//  Created by Jesse Lingeman on 6/6/23.
+//
+
+import Foundation
+
+// https://rud.is/books/swiftr/
