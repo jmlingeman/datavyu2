8400b7cc34f89af2001677dcbfb5b237eb5be405
Jesse Lingeman <jesse@jesseisageek.com>
2023-06-18 18:55:00 -0400

    reimplement weak temporal algorithm from old datavyu

--- datavyu/Views/Layouts/WeakTemporalLayout.swift
+++ datavyu/Views/Layouts/WeakTemporalLayout.swift
@@ -81,6 +81,11 @@ struct WeakTemporalLayout: Layout {
          */
         var sizes: [String: CGSize] = [:]
         var pts: [String: CGPoint] = [:]
+        for subview in cellSubviews {
+            sizes[subview.cellIdx] = CGSize(width: columnSize, height: 0)
+            pts[subview.cellIdx] = CGPoint(x: Int(columnSize) * subview.columnIdx, y: 0)
+        }
+        var times = Set<Int>()
         
         let onsetMap = getSubviewMap(key: OnsetKey.self, subviews: cellSubviews)
         let offsetMap = getSubviewMap(key: OffsetKey.self, subviews: cellSubviews)
@@ -95,100 +100,136 @@ struct WeakTemporalLayout: Layout {
             sizes[subview.cellIdx] = CGSize()
         }
         
+        var columnViews: [Int: [LayoutSubview]] = [:]
+        for subview in cellSubviews {
+            columnViews[subview.columnIdx, default: []].append(subview)
+        }
+        for colIdx in columnViews.keys {
+            columnViews[colIdx] = columnViews[colIdx]?.sorted(by: {$0.onset < $1.onset})
+        }
+        
         for titleView in titleSubviews {
             let pt = CGPoint(x: columnSize * Double(titleView[ColumnKey.self]), y: 0)
             titleView.place(at: pt, proposal: .unspecified)
         }
         
-        for onset in sortedOnsets {
-            let localSubviews = onsetMap[onset, default: []]
-            var prevOffset = localSubviews.map({x in x.offset}).sorted().min() ?? 0
-            
-                        
-            for (idx, subview) in localSubviews.sorted(by: {$0.offset < $1.offset}).enumerated() {
-                
-                if subview.offset > prevOffset {
-                    currentOffsetY[subview.columnIdx] += gapSize
-                    prevOffset = subview.offset
+        /* Go through each column and assign "height" values to onset times.
+         This will determine how much space to allocate between onset times.
+         For each column, create an intermediate map and then merge with the
+         actual map using maximum values.  This is required since a column can have
+         multiple cells with the same onset.
+         Also accumulate all unique times.
+         */
+        
+        var heightMap: [Int: Double] = [:]
+        for colIdx in columnViews.keys {
+            var intermediateMap: [Int: Double] = [:]
+            for cell in columnViews[colIdx]! {
+                let height = cell.sizeThatFits(.unspecified).height
+                let onset = cell.onset
+                intermediateMap[onset] = intermediateMap[onset, default: 0] + height
+                times.insert(cell.onset)
+                if cell.offset > cell.onset {
+                    times.insert(cell.offset)
                 }
-                
-                if subview.onset < currentOffset.max()! {
-                    currentOnsetY[subview.columnIdx] += -gapSize
-                }
-                
-                let pt = CGPoint(x: Double(subview.columnIdx) * columnSize, y: currentOnsetY[subview.columnIdx])
-                let cellHeight = subview.sizeThatFits(.unspecified).height
-                let offsetAdjustment = currentOffsetY[subview.columnIdx] - currentOnsetY[subview.columnIdx]
-                print(subview.cellIdx, idx, "sizethatfits", cellHeight, cellHeight + offsetAdjustment, "current onset y", currentOnsetY[subview.columnIdx], offsetAdjustment)
-
-                print(pt)
-                
-                pts[subview.cellIdx] = pt
-                positionMap[subview.onset] = currentOnsetY[subview.columnIdx]
-                sizes[subview.cellIdx]?.width = columnSize
-                
-                // Update bookkeeping
-                currentOffsetY[subview.columnIdx] = currentOffsetY[subview.columnIdx] + cellHeight
-                currentOnsetY[subview.columnIdx] = currentOnsetY[subview.columnIdx] + cellHeight
-                    
-                currentOnset[subview.columnIdx] = subview.onset
-                currentOffset[subview.columnIdx] = subview.offset
             }
-            
-            // Since we're moving to the next onset, mark the new bottoms
-            let maxOnsetY = currentOnsetY.max()!
-            let maxOffsetY = currentOffsetY.max()!
-            let maxOnset = currentOnset.max()!
-            let maxOffset = currentOffset.max()!
-            for idx in currentOnsetY.indices {
-                currentOnsetY[idx] = maxOnsetY
-//                currentOffsetY[idx] = maxOffsetY
+            for onset in intermediateMap.keys {
+                heightMap[onset] = max(heightMap[onset, default: 0], intermediateMap[onset]!)
             }
         }
         
+        var pos = 0.0
+        var onsetToPos: [Int: Double] = [:]
+        for time in times {
+            onsetToPos[time] = pos
+            pos += heightMap[time, default: 0]
+            pos += gapSize
+        }
+        
+        /* Iterate over all spreadsheet cells and set boundaries using onset and offset maps.
+         Keep a local copy of the position maps.  Since each time value gets a range of positions
+         (starting from onset map's value and ending at the offset's value), update the local copy of
+         the onset map to get positions for cells sharing onsets.
+         */
+        var offsetToPos: [Int: Double] = [:]
+        for colIdx in columnViews.keys {
+            var colHeight = 0.0
+            var onsetMapLocal: [Int: Double] = onsetToPos
+            var prevCell: LayoutSubview?
+            var colCells = columnViews[colIdx]!
+            for (idx, curCell) in colCells.enumerated() {
+                var nextCell: LayoutSubview?
+                if idx+1 < colCells.count {
+                    nextCell = colCells[idx+1]
+                }
+                
+                let onset = curCell.onset
+                let offset = curCell.offset
+                let cellTopY = onsetMapLocal[onset]!
+                
+                var cellHeight = 0.0
+                if onset > offset {
+                    // TODO: Set overlap border here
+                    cellHeight = curCell.sizeThatFits(.unspecified).height
+                } else if nextCell != nil && onset == nextCell!.onset {
+                    if onset != offset || offset == nextCell!.offset {
+                        // TODO: Set overlap border here
+                    }
+                    cellHeight = curCell.sizeThatFits(.unspecified).height
+                } else if nextCell != nil && offset >= nextCell!.onset {
+                    // TODO: Set overlap border here
+                    cellHeight = onsetMapLocal[nextCell!.onset]! - cellTopY;
+                } else {
+                    cellHeight = offsetToPos[offset, default: onsetToPos[offset]!] - cellTopY;
+                }
+                
+                if prevCell != nil && onset - prevCell!.offset == 1 {
+                    sizes[prevCell!.cellIdx]!.height =  cellTopY - sizes[prevCell!.cellIdx]!.height
+                    offsetToPos[offset] = max(offsetToPos[offset] ?? 0, cellTopY)
+                }
+                
+                // fix for edge cases...maybe investigate later
+                cellHeight = max(cellHeight, curCell.sizeThatFits(.unspecified).height);
+                // Set cell boundary
+                pts[curCell.cellIdx]?.y = cellTopY
+                sizes[curCell.cellIdx]?.height = cellHeight
+                
+                // Update local onset map
+                onsetMapLocal[onset] = onsetMapLocal[onset]! + cellHeight
+                
+                // Only do if we're not setting overlap
+                offsetToPos[offset] = max(offsetToPos[offset, default: cellTopY + cellHeight], cellTopY + cellHeight)
+                
+                // Update offset map
+//                if(!curCell.getOverlapBorder()) {
+//                    int adjOff = cellTopY + cellHeight;
+//                    offsetMap.compute(offset, (k, v) -> (v == null) ? adjOff : Math.max(v, adjOff));
+//                }
+                
+                // Update vars
+                colHeight = cellTopY + cellHeight;
+                prevCell = curCell;
+            }
+            
+            cache.maxHeight = max(cache.maxHeight, colHeight)
+        }
+
+        
+        for colIdx in columnViews.keys {
+            let colCells = columnViews[colIdx]!
+            for curCell in colCells {
+                let mapHeight = offsetToPos[curCell.offset, default: -1] - pts[curCell.cellIdx]!.y
+                if sizes[curCell.cellIdx]!.height < mapHeight {
+                    sizes[curCell.cellIdx]?.height = mapHeight
+                }
+                curCell.place(at: pts[curCell.cellIdx]!, proposal: ProposedViewSize(sizes[curCell.cellIdx]!))
+            }
+        }
         
         cache.maxHeight = currentOffsetY.max() ?? 300
         cache.maxWidth = Double(sheetModel.columns.count) * columnSize
-
-        for offset in sortedOffsets {
-            let localSubviews = offsetMap[offset, default: []]
-            
-            for (idx, subview) in localSubviews.sorted(by: {$0.offset < $1.offset}).enumerated() {
-                var pos: Double
-                if positionMap.keys.contains(subview.offset) {
-                    pos = positionMap[subview.offset]!
-                } else {
-                    let closeestPos = closestMatch(values: Array(positionMap.keys), inputValue: subview.offset)!
-                    pos = positionMap[closeestPos]! + gapSize
-                    positionMap[subview.offset] = pos
-                }
-                
-                let pt = pts[subview.cellIdx]!
-                var size = sizes[subview.cellIdx]!
-                
-                let cellHeight: Double
-                if pos - pt.y < 0 {
-                    cellHeight = subview.sizeThatFits(.unspecified).height
-                } else {
-                    cellHeight = pos - pt.y
-                }
-                size.height = cellHeight
-                print(subview.cellIdx, pt, size, cellHeight)
-
-                subview.place(at: pt, proposal: ProposedViewSize(size))
-                
-            }
-        }
-        
-        
-//        for idx in subviews.indices {
-//            subviews[idx].place(at: pt, anchor: .topLeading, proposal: .unspecified)
-//
-//            if idx < subviews.count - 1 {
-//                pt.x += subviews[idx].sizeThatFits(.unspecified).width + cache.spaces[idx]
-//            }
-//        }
     }
+        
     
     func computeSpaces(subviews: LayoutSubviews) -> [CGFloat] {
         if let spacing {
@@ -238,6 +279,10 @@ struct CellIdx: LayoutValueKey {
     static var defaultValue: String = ""
 }
 
+struct Overlap: LayoutValueKey {
+    static var defaultValue: Bool = false
+}
+
 extension LayoutSubview {
     var onset: Int {
         self[OnsetKey.self]
@@ -257,6 +302,10 @@ extension LayoutSubview {
     
     var cellIdx: String {
         self[CellIdx.self]
+    }
+    
+    var overlap: Bool {
+        self[Overlap.self]
     }
 }
 
@@ -279,6 +328,10 @@ extension View {
     
     func setCellIdx(_ cellIdx: String) -> some View {
         layoutValue(key: CellIdx.self, value: cellIdx)
+    }
+    
+    func setOverlap(_ overlap: Bool) -> some View {
+        layoutValue(key: Overlap.self, value: overlap)
     }
     
 }
