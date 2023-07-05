ba667555f0054a3c033c07eae01e758c11600c99
Jesse Lingeman <jesse@jesseisageek.com>
2023-07-05 15:01:04 -0400

    add basic options menu and stub in the first app storage usage

--- datavyu/Views/Options/OptionsView.swift
+++ datavyu/Views/Options/OptionsView.swift
@@ -7,24 +7,24 @@
 
 import SwiftUI
 
-enum ColorOptions {
-    case auto
-    case light
-    case dark
-}
+
 
 struct OptionsView: View {
     @Environment(\.dismiss) var dismiss
-    @State var colorscheme: ColorOptions = ColorOptions.auto
+    
+    @AppStorage("colorscheme") private var colorscheme: String = "auto"
+    
+    
+    let config = Config()
     
     var body: some View {
         ScrollView(.vertical) {
             Text("Options").font(.system(size: 30)).frame(alignment: .topLeading).padding()
             HStack {
                 Picker(selection: $colorscheme, label: Text("Colorscheme:")) {
-                    Text("Match System Setting").tag(ColorOptions.auto)
-                    Text("Light").tag(ColorOptions.light)
-                    Text("Dark").tag(ColorOptions.dark)
+                    Text("Match System Setting").tag("auto")
+                    Text("Light").tag("light")
+                    Text("Dark").tag("dark")
                 }.padding()
             }.padding()
             Button("Close") {
