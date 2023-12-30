053171918592c0558ec8ea532f66b6ccee0fed42
Jesse Lingeman <jesse@jesseisageek.com>
2023-12-30 14:03:40 -0500

    reimpl of layout code in collectionviewlayout

--- datavyu/Views/Layouts/TemporalCollectionViewLayout.swift
+++ datavyu/Views/Layouts/TemporalCollectionViewLayout.swift
@@ -10,20 +10,186 @@ import AppKit
 import SwiftUI
 
 class TemporalCollectionViewLayout: NSCollectionViewLayout {
-    override var collectionViewContentSize: NSSize
+//    override var collectionViewContentSize: NSSize
     @ObservedObject var sheetModel: SheetModel
     
     init(sheetModel: SheetModel, collectionViewContentSize: NSSize) {
         self.sheetModel = sheetModel
-        self.collectionViewContentSize = collectionViewContentSize
+        super.init()
+//        self.collectionViewContentSize = collectionViewContentSize
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    struct CellInfo: Hashable {
+        let model: CellModel
+        let columnIdx: Int
+        let cellIdx: Int
+    }
+    
     override func prepare() {
+        let config = Config()
+        let gapSize = config.gapSize
+        let columnSize = config.defaultCellWidth
+        let headerSize = config.headerSize
         
+        
+        var cellLayouts = [CellInfo: NSCollectionViewLayoutAttributes]()
+        for (colIdx, column) in sheetModel.columns.enumerated() {
+            for (cellIdx, cell) in column.cells.enumerated() {
+                let cellInfo = CellInfo(model: cell, columnIdx: colIdx, cellIdx: cellIdx)
+                cellLayouts[cellInfo] = NSCollectionViewLayoutAttributes()
+            }
+        }
+        
+        /*
+         Idea: Loop through storing proposed positions
+         Loop through again to fix the heights based on placed onsets/offsets
+         */
+        
+        // Bookkeeping Hashmaps
+        var sizes: [CellInfo: CGSize] = [:]
+        var pts: [CellInfo: CGPoint] = [:]
+        var heightMap: [Int: Double] = [:]
+        var onsetToPos: [Int: Double] = [:]
+        var offsetToPos: [Int: Double] = [:]
+        
+        // Assign defaults
+        for subview in cellLayouts.keys {
+            sizes[subview] = CGSize(width: columnSize, height: 0)
+            pts[subview] = CGPoint(x: Int(columnSize) * subview.columnIdx, y: 0)
+        }
+        var times = Set<Int>()
+        
+        var columnViews: [Int: [CellInfo]] = [:]
+        for subview in cellLayouts.keys {
+            columnViews[subview.columnIdx, default: []].append(subview)
+        }
+        for colIdx in columnViews.keys {
+            columnViews[colIdx] = columnViews[colIdx]?.sorted(by: {
+                if $0.model.onset == $1.model.onset {
+                    if $0.model.offset == $1.model.offset {
+                        return $0.cellIdx < $1.cellIdx
+                    }
+                    return $0.model.offset < $1.model.offset
+                }
+                return $0.model.onset < $1.model.onset
+            })
+        }
+        
+        
+        /* Go through each column and assign "height" values to onset times.
+         This will determine how much space to allocate between onset times.
+         For each column, create an intermediate map and then merge with the
+         actual map using maximum values.  This is required since a column can have
+         multiple cells with the same onset.
+         Also accumulate all unique times.
+         */
+        
+        
+        
+        for colIdx in columnViews.keys {
+            var intermediateMap: [Int: Double] = [:]
+            for cell in columnViews[colIdx]! {
+                let height = Double(config.minCellHeight)
+                let onset = cell.model.onset
+                intermediateMap[onset] = intermediateMap[onset, default: 0] + height
+                times.insert(cell.model.onset)
+                if cell.model.offset > cell.model.onset {
+                    times.insert(cell.model.offset)
+                }
+            }
+            for onset in intermediateMap.keys {
+                heightMap[onset] = max(heightMap[onset, default: 0], intermediateMap[onset]!)
+            }
+        }
+        
+        
+        var pos = headerSize
+        for time in Array(times).sorted() {
+            onsetToPos[time] = pos
+            pos += heightMap[time, default: 0]
+            pos += gapSize
+        }
+        
+        /* Iterate over all spreadsheet cells and set boundaries using onset and offset maps.
+         Keep a local copy of the position maps.  Since each time value gets a range of positions
+         (starting from onset map's value and ending at the offset's value), update the local copy of
+         the onset map to get positions for cells sharing onsets.
+         */
+        for colIdx in columnViews.keys {
+            var colHeight = 0.0
+            var onsetMapLocal: [Int: Double] = onsetToPos
+            var prevCell: CellInfo?
+            let colCells = columnViews[colIdx]!
+            for (idx, curCell) in colCells.enumerated() {
+                var nextCell: CellInfo?
+                if idx+1 < colCells.count {
+                    nextCell = colCells[idx+1]
+                }
+                
+                let onset = curCell.model.onset
+                let offset = curCell.model.offset
+                let cellTopY = onsetMapLocal[onset]!
+                
+                var cellHeight = 0.0
+                if onset > offset {
+                    // TODO: Set overlap border here
+                    cellHeight = Double(config.minCellHeight)
+                } else if nextCell != nil && onset == nextCell!.model.onset {
+                    if onset != offset || offset == nextCell!.model.offset {
+                        // TODO: Set overlap border here
+                    }
+                    cellHeight = Double(config.minCellHeight)
+                } else if nextCell != nil && offset >= nextCell!.model.onset {
+                    // TODO: Set overlap border here
+                    cellHeight = onsetMapLocal[nextCell!.model.onset]! - cellTopY;
+                } else {
+                    cellHeight = offsetToPos[offset, default: onsetToPos[offset]!] - cellTopY;
+                }
+                
+                if prevCell != nil && onset - prevCell!.model.offset == 1 {
+                    sizes[prevCell!]!.height =  cellTopY - sizes[prevCell!]!.height
+                    offsetToPos[offset] = max(offsetToPos[offset] ?? 0, cellTopY)
+                }
+                
+                // fix for edge cases...maybe investigate later
+                cellHeight = max(cellHeight, Double(config.minCellHeight));
+                // Set cell boundary
+                pts[curCell]?.y = cellTopY
+                sizes[curCell]?.height = cellHeight
+                
+                // Update local onset map
+                onsetMapLocal[onset] = onsetMapLocal[onset]! + cellHeight
+                
+                // Only do if we're not setting overlap
+                if curCell.model.onset < curCell.model.offset {
+                    offsetToPos[offset] = max(offsetToPos[offset, default: cellTopY + cellHeight], cellTopY + cellHeight)
+                }
+                
+                // Update vars
+                colHeight = cellTopY + cellHeight;
+                prevCell = curCell;
+            }
+            
+//            cache.maxHeight = max(cache.maxHeight, colHeight)
+        }
+        
+        
+        for colIdx in columnViews.keys {
+            let colCells = columnViews[colIdx]!
+            for curCell in colCells {
+                let mapHeight = offsetToPos[curCell.model.offset, default: -1] - pts[curCell]!.y
+                if sizes[curCell]!.height < mapHeight {
+                    sizes[curCell]?.height = mapHeight
+                }
+//                curCell.place(at: pts[curCell]!, proposal: ProposedViewSize(sizes[curCell]!))
+            }
+        }
+        
+//        cache.maxWidth = Double(sheetModel.columns.count) * columnSize
     }
     
     override func layoutAttributesForElements(in rect: NSRect) -> [NSCollectionViewLayoutAttributes] {
