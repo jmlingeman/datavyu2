8f00f7eb1c3e6704025ec1caad24342208bed91c
Jesse Lingeman <jesse@jesseisageek.com>
2024-01-03 19:57:31 -0500

    Fix headers, improve scroll perf a bit.

---
+++ datavyu/Views/Components/WrappedHStack.swift
@@ -0,0 +1,18 @@
+//
+//  WrappedHStack.swift
+//  datavyu
+//
+//  Created by Jesse Lingeman on 1/3/24.
+//
+
+import SwiftUI
+
+struct WrappedHStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WrappedHStack()
+}
