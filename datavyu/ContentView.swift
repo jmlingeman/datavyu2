19333c31a3ae9e75d13fc85a31435aaa9f4cba6a
Jesse Lingeman <jesse@jesseisageek.com>
2024-05-10 17:57:20 -0400

    Fixes toolbar duplication

--- datavyu/ContentView.swift
+++ datavyu/ContentView.swift
@@ -12,22 +12,90 @@ import WrappingHStack
 struct ContentView: View {
     @EnvironmentObject var fileController: FileControllerModel
     @State var selectedTab = 0
+    
+    @State private var showingCodeEditor = false
+    @State private var showingOptions = false
+    @State private var showingColumnList = false
+    @State private var layoutLabel = "Ordinal Layout"
+    @State private var temporalLayout = false
+    @State private var hideLabel = "Hide Controller"
+    @State private var hideController = false
 
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            ForEach(Array(zip(fileController.fileModels.indices, $fileController.fileModels)), id: \.0) { idx, $fileModel in
-                DatavyuView(fileModel: fileModel).tabItem { Text(fileModel.sheetModel.sheetName) }
-                    .environmentObject(fileModel.sheetModel).tag(idx)
+        GeometryReader { gr in
+            TabView(selection: $selectedTab) {
+                ForEach(Array(zip(fileController.fileModels.indices, $fileController.fileModels)), id: \.0) { idx, $fileModel in
+                    DatavyuView(fileModel: fileModel, temporalLayout: $temporalLayout, hideController: $hideController).tabItem { Text(fileModel.sheetModel.sheetName) }
+                        .environmentObject(fileModel.sheetModel).tag(idx)
+                }
             }
-        }
-        .onAppear {
-            let server = DatavyuAPIServer(fileController: fileController, port: 1312)
-            server.start()
-        }
-        .onChange(of: fileController.activeFileModel) { oldValue, newValue in
-            let newTabIdx = fileController.fileModels.firstIndex(of: newValue)
-            selectedTab = newTabIdx ?? 0
+            .onAppear {
+                let server = DatavyuAPIServer(fileController: fileController, port: 1312)
+                server.start()
+            }
+            .onChange(of: fileController.activeFileModel) { oldValue, newValue in
+                let newTabIdx = fileController.fileModels.firstIndex(of: newValue)
+                selectedTab = newTabIdx ?? 0
+            }
+            .toolbar {
+                ToolbarItemGroup {
+                    Button("Code Editor") {
+                        showingCodeEditor.toggle()
+                    }
+                    .sheet(isPresented: $showingCodeEditor) {
+                        CodeEditorView(fileModel: fileController.activeFileModel).frame(width: gr.size.width / 2, height: gr.size.height / 2)
+                    }
+                    Button("Options") {
+                        showingOptions.toggle()
+                    }
+                    .sheet(isPresented: $showingOptions) {
+                        OptionsView().frame(width: gr.size.width / 2, height: gr.size.height / 2)
+                    }
+                    Button("Hide/Show Columns") {
+                        showingColumnList.toggle()
+                    }
+                    .sheet(isPresented: $showingColumnList) {
+                        ColumnListView(sheetModel: fileController.activeFileModel.sheetModel).frame(width: gr.size.width / 2, height: gr.size.height / 2)
+                    }
+                    Button("Open File")
+                    {
+                        let panel = NSOpenPanel()
+                        panel.allowsMultipleSelection = false
+                        panel.canChooseDirectories = false
+                        if panel.runModal() == .OK {
+                            fileController.openFile(inputFilename: panel.url!)
+                        }
+                    }
+                    Button("Save File")
+                    {
+                        let panel = NSSavePanel()
+                        if panel.runModal() == .OK {
+                            let _ = saveOpfFile(fileModel: fileController.activeFileModel, outputFilename: panel.url!)
+                        }
+                    }
+                    Button(layoutLabel) {
+                        if layoutLabel == "Ordinal Layout" {
+                            layoutLabel = "Temporal Layout"
+                            temporalLayout = true
+                        } else {
+                            layoutLabel = "Ordinal Layout"
+                            temporalLayout = false
+                        }
+                    }.keyboardShortcut("t")
+                    Button(hideLabel) {
+                        withAnimation {
+                            if hideLabel == "Hide Controller" {
+                                hideLabel = "Show Controller"
+                                hideController = true
+                            } else {
+                                hideLabel = "Hide Controller"
+                                hideController = false
+                            }
+                        }
+                    }.keyboardShortcut("g")
+                }
+            }
         }
         
     }
--- datavyu/Views/DatavyuView.swift
+++ datavyu/Views/DatavyuView.swift
@@ -9,14 +9,9 @@ import SwiftUI
 
 struct DatavyuView: View {
     @ObservedObject var fileModel: FileModel
-    @State private var showingCodeEditor = false
-    @State private var showingOptions = false
-    @State private var showingColumnList = false
-    @State private var layoutLabel = "Ordinal Layout"
-    @State private var temporalLayout = false
-    @State private var hideLabel = "Hide Controller"
-    @State private var hideController = false
 
+    @Binding var temporalLayout: Bool
+    @Binding var hideController: Bool
     
     @EnvironmentObject var fileController: FileControllerModel
     @Environment(\.undoManager) var undoManager
@@ -29,64 +24,6 @@ struct DatavyuView: View {
                         fileModel.sheetModel.setUndoManager(undoManager: undoManager!)
                     })
                     .environmentObject(fileModel.sheetModel)
-                    .toolbar {
-                        ToolbarItemGroup {
-                            Button("Code Editor") {
-                                showingCodeEditor.toggle()
-                            }
-                            .sheet(isPresented: $showingCodeEditor) {
-                                CodeEditorView(fileModel: fileModel).frame(width: gr.size.width / 2, height: gr.size.height / 2)
-                            }
-                            Button("Options") {
-                                showingOptions.toggle()
-                            }
-                            .sheet(isPresented: $showingOptions) {
-                                OptionsView().frame(width: gr.size.width / 2, height: gr.size.height / 2)
-                            }
-                            Button("Hide/Show Columns") {
-                                showingColumnList.toggle()
-                            }
-                            .sheet(isPresented: $showingColumnList) {
-                                ColumnListView(sheetModel: fileModel.sheetModel).frame(width: gr.size.width / 2, height: gr.size.height / 2)
-                            }
-                            Button("Open File")
-                            {
-                                let panel = NSOpenPanel()
-                                panel.allowsMultipleSelection = false
-                                panel.canChooseDirectories = false
-                                if panel.runModal() == .OK {
-                                    fileController.openFile(inputFilename: panel.url!)
-                                }
-                            }
-                            Button("Save File")
-                            {
-                                let panel = NSSavePanel()
-                                if panel.runModal() == .OK {
-                                    saveOpfFile(fileModel: fileModel, outputFilename: panel.url!)
-                                }
-                            }
-                            Button(layoutLabel) {
-                                if layoutLabel == "Ordinal Layout" {
-                                    layoutLabel = "Temporal Layout"
-                                    temporalLayout = true
-                                } else {
-                                    layoutLabel = "Ordinal Layout"
-                                    temporalLayout = false
-                                }
-                            }.keyboardShortcut("t")
-                            Button(hideLabel) {
-                                withAnimation {
-                                    if hideLabel == "Hide Controller" {
-                                        hideLabel = "Show Controller"
-                                        hideController = true
-                                    } else {
-                                        hideLabel = "Hide Controller"
-                                        hideController = false
-                                    }
-                                }
-                            }.keyboardShortcut("g")
-                        }
-                    }
             }
         }
         
