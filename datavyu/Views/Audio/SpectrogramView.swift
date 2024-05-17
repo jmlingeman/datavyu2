703b1cdd339b6d4afccd822381400b23c3073a82
Jesse Lingeman <jesse@jesseisageek.com>
2024-05-17 17:31:06 -0400

    Spectrogram WIP. It is functional now, needs work.

---
+++ datavyu/Views/Audio/SpectrogramView.swift
@@ -0,0 +1,18 @@
+//
+//  SpectrogramView.swift
+//  datavyu
+//
+//  Created by Jesse Lingeman on 5/15/24.
+//
+
+import SwiftUI
+
+struct SpectrogramView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpectrogramView()
+}
---
+++ datavyu/Visualizations/SpectrogramController.swift
@@ -0,0 +1,8 @@
+//
+//  SpectrogramController.swift
+//  datavyu
+//
+//  Created by Jesse Lingeman on 5/14/24.
+//
+
+import Foundation
