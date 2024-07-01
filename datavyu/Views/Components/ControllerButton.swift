16f6a07e5369c103c2fdd464824fdef1e513d5b9
Jesse Lingeman <jesse@jesseisageek.com>
2024-06-30 22:40:19 -0400

    Prettification: Images for controller buttons

--- datavyu/Views/Components/ControllerButton.swift
+++ datavyu/Views/Components/ControllerButton.swift
@@ -7,13 +7,53 @@
 
 import SwiftUI
 
+struct ControllerButtonLabel: View {
+    var buttonName: String
+
+    var body: some View {
+        switch buttonName {
+        case "Set\nOnset":
+            Image(systemName: "arrow.up.backward.bottomtrailing.rectangle")
+        case "Set\nOffset":
+            Image(systemName: "arrow.up.right.bottomleft.rectangle")
+        case "Play":
+            Image(systemName: "play")
+        case "Stop":
+            Image(systemName: "stop")
+        case "Shuttle <":
+            Image(systemName: "backward")
+        case "Shuttle >":
+            Image(systemName: "forward")
+        case "Pause":
+            Image(systemName: "pause")
+        case "Prev":
+            Image(systemName: "backward.frame")
+        case "Next":
+            Image(systemName: "forward.frame")
+        case "Add\nCell":
+            Image(systemName: "plus.rectangle")
+        case "Set\nOffset\n+ Add":
+            HStack {
+                Image(systemName: "arrow.up.right.bottomleft.rectangle")
+                Image(systemName: "plus.rectangle")
+            }
+        case "Jump":
+            Image(systemName: "arrowshape.turn.up.backward")
+        case "Find":
+            Image(systemName: "magnifyingglass")
+        default:
+            Text(buttonName)
+        }
+    }
+}
+
 struct ControllerButton: View {
     var buttonName: String
     var action: () -> Void
 
     var body: some View {
         Button(action: action) {
-            Text(buttonName).font(.system(size: 10))
+            ControllerButtonLabel(buttonName: buttonName).font(.system(size: 20))
                 .multilineTextAlignment(.center)
                 .padding()
                 .background(RoundedRectangle(cornerRadius: 8).fill(Color.secondary))
