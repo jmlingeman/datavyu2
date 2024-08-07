f77562efc50686ff236039abeb49468ed86d317a
Jesse Lingeman <jesse@jesseisageek.com>
2024-08-07 19:32:26 -0400

    Start fixing up code editor.

--- datavyu/Views/CodeEditor/CodeEditorRow.swift
+++ datavyu/Views/CodeEditor/CodeEditorRow.swift
@@ -9,20 +9,28 @@ import SwiftUI
 
 struct CodeEditorRow: View {
     @ObservedObject var column: ColumnModel
+    @State var selectedArgument: Argument?
 
     var body: some View {
-//        HStack {
-        EditableLabel($column.columnName)
-        WrappedHStack($column.arguments) { $argument in
-//            ForEach($column.arguments) { $argument in
-            TextField(argument.name, text: $argument.name)
-                .frame(maxWidth: 100)
-                .padding()
-                .border(Color.black, width: 5)
+        VStack {
+            HStack {
+                EditableLabel($column.columnName)
+//                WrappedHStack($column.arguments) { $argument in
+//                    TextField(argument.name, text: $argument.name)
+//                        .frame(maxWidth: 100)
+//                        .padding()
+//                }.border(Color.primary, width: 2)
+                CodeRowTextFieldView(column: column)
+
+                CodeEditorAddCodeButton(column: column)
+                CodeEditorRemoveCodeButton(column: column)
+                Spacer()
+            }
+            HStack {
+                Spacer()
+//                CodeEditorMoveCodeButton(column: column, direction: "left", code: selectedArgument)
+//                CodeEditorMoveCodeButton(column: column)
+            }
         }
-        CodeEditorAddCodeButton(column: column)
-        CodeEditorRemoveCodeButton(column: column)
-        Spacer()
-//        }.padding()
     }
 }
